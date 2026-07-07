adefcdd0bff3411386c5d5552b65e38148f346ea
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-07-07 19:13:14 +0330

    update(OtpController): add verify method

--- laravel/app/Http/Controllers/Auth/OtpController.php
+++ laravel/app/Http/Controllers/Auth/OtpController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\OtpSendRequest;
+use App\Http\Requests\Auth\VerifyPhoneRequest;
 use App\Models\OtpCode;
 use App\Services\OtpService;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Str;
 
@@ -26,16 +26,22 @@ class OtpController extends Controller
             operation: OtpCode::PURPOSE[$otp->purpose],
             code: $otp->code,
         );
-        $otp->service_response = $result['body']['data']['messageId'] ?? -1;
-        $otp->save();
 
-        return response()->json(
-            [
-                'message'   => Lang::get($result['success'] ? 'auth.otp.send.success' : 'auth.otp.send.fail'),
-                'expires_at' => $result['success'] && $otp->created_at->addMinutes(config('services.otp.timeout')),
-            ],
-            Lang::get($result['success'] ? 200 : 503)
-        );
+        $otp->update([
+            'service_response' => $result['body']['data']['messageId'] ?? -1,
+        ]);
+
+        $response = [
+            'message' => Lang::get($result['success']
+                ? 'auth.otp.send.success'
+                : 'auth.otp.send.fail'),
+        ];
+
+        if ($result['success']) {
+            $response['expires_at'] = $otp->created_at->addSeconds(config('auth.otp.expiry'));
+        }
+
+        return response()->json($response, $result['success'] ? 200 : 503);
     }
 
     public function send(OtpSendRequest $request)
@@ -58,5 +64,37 @@ class OtpController extends Controller
             'code'    => random_int(100000, 999999),
             'purpose' => $purpose,
         ]));
+    }
+
+    public function verify(VerifyPhoneRequest $request)
+    {
+        $otp = OtpCode
+            ::where('phone', $request->validated('phone'))
+            ->where('purpose', $request->validated('purpose'))
+            ->isValid()->first();
+
+        if (!$otp) {
+            return response()->json([
+                'message' => Lang::get('auth.otp.verify.invalid')
+            ], 422);
+        }
+
+        $otp->increment('attempts');
+
+        if (!hash_equals($otp->code, $request->validated('code'))) {
+            return response()->json([
+                'message' => Lang::get('auth.otp.verify.incorrect')
+            ], 422);
+        }
+
+        $otp->update([
+            'used_at' => now(),
+            'verification_token' => hash('sha256', $token = Str::random(40))
+        ]);
+
+        return response()->json([
+            'verification_token' => $token,
+            'expires_in' => config('auth.otp.token_expiry')
+        ], 200);
     }
 }
