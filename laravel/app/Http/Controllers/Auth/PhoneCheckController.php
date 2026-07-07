fc878d81b34d0aa1a351b9b7d893787adaea06ae
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-07-07 16:17:55 +0330

    add(PhoneCheckController.php)

---
+++ laravel/app/Http/Controllers/Auth/PhoneCheckController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\CheckPhoneRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Lang;
+
+class PhoneCheckController extends Controller
+{
+    public function __invoke(CheckPhoneRequest $request)
+    {
+        $phone = $request->validated('phone');
+        $exists = User::where('phone', $phone)->exists();
+
+        return response()->json([
+            'available' => ! $exists,
+            'message'   => Lang::get(
+                $exists
+                    ? 'auth.phone.taken'
+                    : 'auth.phone.available'
+            )
+        ], $exists ? 200 : 404);
+    }
+}
