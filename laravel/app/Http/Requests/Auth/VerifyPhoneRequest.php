1f5216d39efe547b83643c7c19ae83bcdeebb071
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-07-07 19:00:51 +0330

    add(VerifyPhoneRequest.php)

---
+++ laravel/app/Http/Requests/Auth/VerifyPhoneRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use App\Helpers\SanitizeHelper;
+use App\Http\Requests\ApiRequest;
+
+class VerifyPhoneRequest extends ApiRequest
+{
+    public function rules(): array
+    {
+        return [
+            'phone' => $this->getRule('phone'),
+            'code' => $this->getRule('code'),
+            'purpose' => $this->getRule('purpose'),
+        ];
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'phone' => SanitizeHelper::sanitizePhone($this->phone),
+        ]);
+    }
+
+    public function validatedUserData(): array
+    {
+        return [
+            'phone' => $this->validated('phone'),
+        ];
+    }
+}
