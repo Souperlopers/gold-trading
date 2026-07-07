dd661a512f56a12969010f5fa956510f81dc2c4b
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-07-07 16:27:49 +0330

    add(CheckPhoneTest.php)

---
+++ laravel/tests/Feature/Auth/CheckPhoneTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\User;
+
+class CheckPhoneTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private string $endpoint = '/api/auth/phone-check';
+
+    public function test_phone_is_available()
+    {
+        $response = $this->postJson($this->endpoint, [
+            'phone' => '09123456789',
+        ]);
+
+        $response->assertOk()->assertJson([
+            'available' => true,
+        ]);
+    }
+
+    public function test_phone_is_taken()
+    {
+        User::factory()->create([
+            'phone' => '09123456789',
+            'national_id' => '1362964441',
+        ]);
+
+        $response = $this->post($this->endpoint, [
+            'phone' => '09123456789',
+        ]);
+
+        $response->assertOk()->assertJson([
+            'available' => false,
+        ]);
+    }
+
+    public function test_invalid_phone_format_is_rejected()
+    {
+        $response = $this->postJson($this->endpoint, [
+            'phone' => '12345',
+        ]);
+
+        $response->assertUnprocessable()
+            ->assertJsonValidationErrors(['phone']);
+    }
+
+    public function test_empty_phone_is_rejected()
+    {
+        $response = $this->postJson($this->endpoint, [
+            'phone' => '',
+        ]);
+
+        $response->assertUnprocessable()
+            ->assertJsonValidationErrors(['phone']);
+    }
+
+    public function test_missing_phone_field_is_rejected()
+    {
+        $response = $this->postJson($this->endpoint, []);
+
+        $response->assertUnprocessable()
+            ->assertJsonValidationErrors(['phone']);
+    }
+
+    public function test_sanitisation_works_correctly()
+    {
+        // Send phone with spaces, dashes, etc.
+        $response = $this->postJson($this->endpoint, [
+            'phone' => '+98 915 123 4567',
+        ]);
+
+        // The sanitised number should be 09151234567
+        $response->assertOk()
+            ->assertJson(['available' => true]);
+
+        // Now create a user with the sanitised number
+        User::factory()->create(['phone' => '09151234567']);
+
+        $response = $this->postJson($this->endpoint, [
+            'phone' => '+98 915 123 4567',
+        ]);
+
+        $response->assertOk()
+            ->assertJson(['available' => false]);
+    }
+}
