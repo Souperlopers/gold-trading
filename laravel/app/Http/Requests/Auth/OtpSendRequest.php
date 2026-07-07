1003d55cebbb5469d723b290be9b36003c569d19
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-07-07 16:49:20 +0330

    add(OtpSendRequest)

---
+++ laravel/app/Http/Requests/Auth/OtpSendRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use App\Helpers\SanitizeHelper;
+use App\Http\Requests\ApiRequest;
+
+class OtpSendRequest extends ApiRequest
+{
+    public function rules(): array
+    {
+        return [
+            'phone' => $this->getRule('phone'),
+            'purpose' => $this->getRule('purpose'),
+        ];
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'phone' => SanitizeHelper::sanitizePhone($this->phone),
+        ]);
+    }
+
+    public function validatedUserData(): array
+    {
+        return [
+            'phone' => $this->validated('phone'),
+        ];
+    }
+}
