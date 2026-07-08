b5f2f37e4ea7b03647a3932493937bb46f11387c
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-07-08 23:13:15 +0330

    update(ApiRequest): add other validation rules

--- laravel/app/Http/Requests/ApiRequest.php
+++ laravel/app/Http/Requests/ApiRequest.php
@@ -4,18 +4,22 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use App\Models\OtpCode;
+use App\Rules\NationalIdRule;
+use Illuminate\Validation\Rules\Password;
 
 class ApiRequest extends FormRequest
 {
     protected function getRule(string $key)
     {
         return [
-            'phone' => ['required', 'starts_with:09', 'size:11', 'regex:/^09\d{9}$/'],
-
-            // otp code purpose
-            'purpose' => ['required', 'in:' . implode(',', array_keys(OtpCode::PURPOSE))],
-
-            'code' => ['required', 'digits:6'],
+            'name'        => ['required', 'string', 'min:3', 'max:191'],
+            'phone'       => ['required', 'starts_with:09', 'size:11', 'regex:/^09\d{9}$/'],
+            'national_id' => ['required', 'unique:users,national_id', 'size:10', new NationalIdRule()],
+            'code'        => ['required', 'digits:6'],
+            'purpose'     => ['required', 'in:' . implode(',', array_keys(OtpCode::PURPOSE))],
+            'password'    => ['required', 'confirmed', Password::min(8)->mixedCase()->numbers()],
+            'otp_token'   => ['required', 'string', 'size:40', 'exists:otp_codes,verification_token'],
+            'client'      => ['required', 'string', 'in:mobile,web'],
         ][$key];
     }
 }
