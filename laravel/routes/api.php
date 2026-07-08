9e4e8a18c30e14eda98f1f01b1df6e8c54058ccc
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-07-08 23:19:25 +0330

    update(routes/api): add login related routes

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegistrationController;
 use App\Http\Controllers\Auth\OtpController;
 use App\Http\Controllers\Auth\PhoneCheckController;
 use Illuminate\Support\Facades\Route;
@@ -8,4 +11,8 @@ Route::prefix('/auth')->group(function () {
     Route::get('/phone', PhoneCheckController::class);
     Route::post('/phone/send-otp', [OtpController::class, 'send']);
     Route::post('/phone/verify', [OtpController::class, 'verify']);
+
+    Route::post('/register', [RegistrationController::class, 'register']);
+    Route::post('/login', [LoginController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 })->middleware('throttle:10,1');
