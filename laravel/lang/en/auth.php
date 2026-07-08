e7a526e3ae4f51d91346eff5eaedb629be14c552
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-07-08 23:17:43 +0330

    update(lang/en/auth): add default fields

--- laravel/lang/en/auth.php
+++ laravel/lang/en/auth.php
@@ -29,4 +29,6 @@ return [
             'incorrect' => 'Incorrect code.',
         ],
     ],
+    'password'     => 'The provided password is incorrect.',
+    'throttle'     => 'Too many login attempts. Please try again in :seconds seconds.',
 ];
