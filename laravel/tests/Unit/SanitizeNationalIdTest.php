4cc679b7684526354eafeb84a83804ba94f1610b
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-07-10 03:29:10 +0330

    update(SanitizeNationalIdTest): add zero prepending sanitization tests

--- laravel/tests/Unit/SanitizeNationalIdTest.php
+++ laravel/tests/Unit/SanitizeNationalIdTest.php
@@ -7,14 +7,20 @@ use PHPUnit\Framework\TestCase;
 
 class SanitizeNationalIdTest extends TestCase
 {
+    public function test_too_short_code_prepends_with_zero()
+    {
+        $this->assertEquals('0042021332', SanitizeHelper::sanitizeNationalCode('42021332'));
+        $this->assertEquals('0042021332', SanitizeHelper::sanitizeNationalCode('042021332'));
+    }
+
     public function test_correct_code_returns_unchaanged()
     {
-        $this->assertEquals('0052011739', SanitizeHelper::sanitizeNationalCode('0052011739')); // 7 digits
+        $this->assertEquals('0052011739', SanitizeHelper::sanitizeNationalCode('0052011739'));
     }
 
     public function test_too_long_code_returns_unchaanged()
     {
-        $this->assertEquals('00052011739', SanitizeHelper::sanitizeNationalCode('00052011739')); // 7 digits
+        $this->assertEquals('00052011739', SanitizeHelper::sanitizeNationalCode('00052011739'));
     }
 
     public function test_removes_dashes_spaces_and_underscores()
