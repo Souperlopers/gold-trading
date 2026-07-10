b9d93c5195f26bf5f53616c3c62bd5e6c52a29bd
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-07-10 16:24:17 +0330

    update(api.php): add reset-password route

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Auth\RegistrationController;
 use App\Http\Controllers\Auth\OtpController;
@@ -18,6 +19,7 @@ Route::middleware('throttle:10,1')->group(function () {
         Route::post('/register', [RegistrationController::class, 'register']);
         Route::post('/login', [AuthController::class, 'login']);
         Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+        Route::post('/reset-password', [PasswordController::class, 'reset'])->middleware('auth:sanctum');
     });
 
     Route::middleware('auth:sanctum')->prefix('/user')->group(function () {
