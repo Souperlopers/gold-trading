e5bfbc7c8a80d5abcc1748162ae5745943ae3cdf
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-07-10 03:30:12 +0330

    add(RegistrationTest)

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -16,3 +16,7 @@ Route::prefix('/auth')->group(function () {
     Route::post('/login', [LoginController::class, 'login']);
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 })->middleware('throttle:10,1');
+
+Route::prefix('/user')->group(function () {
+    Route::get('/', [AuthController::class, 'user']);
+})->middleware(['auth:sanctum', 'throttle:5,1']);
