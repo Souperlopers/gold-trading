f043ed3d84358d8f4987319f73a48615bdacbb96
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-07-10 16:52:40 +0330

    add(SimpleRequest)

--- laravel/app/Http/Requests/Auth/SimpleRequest.php
+++ laravel/app/Http/Requests/Auth/SimpleRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Auth;
 
 use App\Http\Requests\ApiRequest;
 
-class LogoutRequest extends ApiRequest
+class SimpleRequest extends ApiRequest
 {
     public function rules(): array
     {
