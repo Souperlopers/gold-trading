8c25656fede7bfd793f334f07ac2062ddb25b59b
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-14 18:28:08 +0330

    update (NavigationMenu): add items and set style

--- nextjs/app/_components/main/NavigationMenu.tsx
+++ nextjs/app/_components/main/NavigationMenu.tsx
@@ -1,3 +1,22 @@
 export default function NavigationMenu() {
-	return <nav>this is nav menu</nav>
+	const navigationList = [
+		{title: "خانه", href:""},
+		{title: "گزارشات", href:""},
+		{title: "شارژ", href:""},
+		{title: "حواله", href:""},
+		{title: "تنظیمات", href:""},
+	]	
+
+	const isLastItem = (index:number) => index+1 === navigationList.length;
+	
+
+	return(
+		<div dir="rtl" className="fixed px-5 md:right-48 md:top-5.5 bottom-0 right-0 md:w-2/3 w-full h-12 md:border-none border-t border-x rounded-t-xl bg-surface border-border">
+			<div className="flex md:justify-start justify-between items-center md:gap-10 gap-0 h-full">
+				{navigationList.map((item,index)=>(
+					<li key={item.title} className={`${isLastItem(index) ? "": "md:border-none border-l-2 border-border md:pl-0 sm:pl-10 pl-5"} list-none cursor-pointer text-text-primary hover:text-accent-dark`}>{item.title}</li>
+				))}
+			</div>
+		</div>
+	)
 }
