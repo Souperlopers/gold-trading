1af01d92f6046292d4edbe23ff63651615c159af
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-15 16:39:03 +0330

    add (Providers): add theme provider in it

---
+++ nextjs/app/_providers/Providers.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ThemeProvider from "./themeProvider";
+
+export default function Providers({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        {children}
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+}
