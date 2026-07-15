fe32e4ab73e7a5abef58310b88b3d3305cd04e1f
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-15 16:40:41 +0330

    style(globals.css): add style to don't show top and bottom arrow in number type input

--- nextjs/app/_store/store.ts
+++ nextjs/app/_store/store.ts
@@ -2,11 +2,14 @@ import { configureStore } from "@reduxjs/toolkit"
 import appReducer from "./appSlice"
 import themeReducer from "./themeSlice";
 
+// states
 export const store = configureStore({
 	reducer: {
 		theme: themeReducer,
 	},
 })
 
+//get state
 export type RootState = ReturnType<typeof store.getState>
+//change state
 export type AppDispatch = typeof store.dispatch
