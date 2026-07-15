8f9faf482871623527e7d35dc18ca197dea98764
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-15 11:17:41 +0330

    add (ThemeProvider)

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -2,9 +2,8 @@ import Button from "./_components/ui/Button"
 
 export default function Home() {
 	return (
-		<main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-			<Button title="Test" />
-			<button className="bg-red-500">123</button>
+		<main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
+		Hello Role
 		</main>
 	)
 }
