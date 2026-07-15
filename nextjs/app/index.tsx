df8089f8233dde9abd818b30b8f73ca1ca8618a5
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-15 09:54:37 +0330

    update (index): add theme switch

--- nextjs/app/index.tsx
+++ nextjs/app/index.tsx
@@ -2,3 +2,4 @@
 export {default as Header} from "@/app/_components/main/Header";
 export {default as Navigation} from "@/app/_components/main/NavigationMenu";
 export {default as Logo} from "@/app/_components/main/Logo";
+export {default as ThemeSwitch} from "@/app/_components/main/ThemeSwitcher";
