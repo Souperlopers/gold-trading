c717590bcb1c58dd2a889da23f663f7893ae63ea
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-07-17 00:24:27 +0330

    fixed shrinking logo

--- nextjs/app/_components/main/Logo.tsx
+++ nextjs/app/_components/main/Logo.tsx
@@ -1,6 +1,6 @@
 export default function Logo() {
 	return (
-		<div className="w-12.5 h-12.5 bg-primary rounded-full">
+		<div className="w-12.5 h-12.5 bg-primary rounded-full shrink-0">
 		</div>
 	)
 }
