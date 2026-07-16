83e7b64e2b0c8774a24393fc04f345797a8221c2
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-07-16 22:35:33 +0330

    changed RegistrationController int o UserController

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -1,16 +1,30 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers;
 
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Resources\UserResource;
+use Illuminate\Http\Request;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
 use App\Http\Controllers\Controller;
 use App\Models\OtpCode;
 use Illuminate\Support\Facades\Lang;
 
-class RegistrationController extends Controller
+class UserController extends Controller
 {
-    public function register(RegisterRequest $request)
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(RegisterRequest $request)
     {
         // extract mobile from verification token received from otp
         if (!$verifiedPhone = OtpCode::getPhoneByToken($request->validated('otp_token'))) {
@@ -30,9 +44,40 @@ class RegistrationController extends Controller
         //
 
         // create new user
-        $user = User::create(['phone' => $verifiedPhone, ...$request->registerData()]);
+        $user = new User([
+            "name" => $request->validated('name'),
+            "phone" => $verifiedPhone,
+            "national_id" => $request->validated('national_id'),
+        ]);
+        $user->password = $request->validated('password');
+        $user->role = 'viewer';
+        $user->save();
 
         // start authentication process
         return (new AuthController)->authWithPhonePass($request, $user);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
