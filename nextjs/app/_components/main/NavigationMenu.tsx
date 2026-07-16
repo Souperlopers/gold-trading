b8b9fc0ef1922164be579b2e3182eec8bc8b13b2
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-07-16 23:57:16 +0330

    used better tags in Navigation component

--- nextjs/app/_components/main/NavigationMenu.tsx
+++ nextjs/app/_components/main/NavigationMenu.tsx
@@ -11,12 +11,12 @@ export default function NavigationMenu() {
 	
 
 	return(
-		<div dir="rtl" className="fixed px-5 md:right-48 md:top-5.5 bottom-0 right-0 md:w-2/3 w-full h-12 md:border-none border-t border-x rounded-t-xl bg-surface border-border">
-			<div className="flex md:justify-start justify-between items-center md:gap-10 gap-0 h-full">
+		<nav className="fixed px-5 md:right-48 md:top-5.5 bottom-0 right-0 md:w-2/3 w-full h-12 md:border-none border-t border-x rounded-t-xl bg-surface border-border">
+			<ul className="flex md:justify-start justify-between items-center md:gap-10 gap-0 h-full">
 				{navigationList.map((item,index)=>(
 					<li key={item.title} className={`${isLastItem(index) ? "": "md:border-none border-l-2 border-border md:pl-0 sm:pl-10 pl-5"} list-none cursor-pointer text-text-primary hover:text-accent-dark`}>{item.title}</li>
 				))}
-			</div>
-		</div>
+			</ul>
+		</nav>
 	)
 }
