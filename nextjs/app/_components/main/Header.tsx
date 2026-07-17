02a5d7bb32fedbe07a2777c587a3a55e8611a0ae
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-07-17 08:48:39 +0330

    increased header spacing

--- nextjs/app/_components/main/Header.tsx
+++ nextjs/app/_components/main/Header.tsx
@@ -4,7 +4,7 @@ import Navigation from "@/app/_components/main/NavigationMenu";
 export default function Header() {
 	return (
 		<header dir="rtl" className="bg-surface h-18.75 fixed w-full border-b border-x rounded-b-xl border-border flex justify-center">
-			<div className="px-5 flex items-center justify-between gap-7 w-full h-full max-w-360 center">
+			<div className="px-10 flex items-center justify-between gap-16 w-full h-full max-w-360 center">
 				<Logo />
 				<Navigation />
 				<ThemeSwitch />
