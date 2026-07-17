2074b66a9f012296b5f29e629e544e10fa4ddfce
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-07-17 08:47:05 +0330

    fixed navlist text disarray in mobile mode

--- nextjs/app/_components/main/NavigationMenu.tsx
+++ nextjs/app/_components/main/NavigationMenu.tsx
@@ -3,26 +3,33 @@ import Link from "next/link"
 export default function NavigationMenu() {
 	const navigationList = [
 		{ title: "خانه", href: "" },
+		{ separator: true },
 		{ title: "گزارشات", href: "" },
+		{ separator: true },
 		{ title: "شارژ", href: "" },
+		{ separator: true },
 		{ title: "حواله", href: "" },
+		{ separator: true },
 		{ title: "تنظیمات", href: "" },
 	]
 
-	const isLastItem = (index: number) => index + 1 === navigationList.length
-
 	return (
 		<nav className="fixed bottom-0 right-0 w-full h-12 border-t border-x rounded-t-xl bg-surface border-border md:static md:border-none">
-			<ul className="flex md:justify-start justify-between items-center md:gap-10 gap-0 h-full">
-				{navigationList.map((item, index) => (
-					<Link
-						key={item.title}
-						href={item.href}
-						className={`${isLastItem(index) ? "" : "md:border-none border-l-2 border-border md:pl-0 sm:pl-10 pl-5"} list-none cursor-pointer text-text-primary hover:text-accent-dark`}
-					>
-						{item.title}
-					</Link>
-				))}
+			<ul className="flex justify-around md:justify-start md:justify-between items-center md:gap-10 gap-0 h-full">
+				{navigationList.map((item, index) =>
+					item.separator ? (
+						<div
+							key={index}
+							className="md:border-none h-2/3 border-border border-l-2"
+						></div>
+					) : (
+						<li key={index}>
+							<Link href={item.href} className="list-none text-text-primary">
+								{item.title}
+							</Link>
+						</li>
+					),
+				)}
 			</ul>
 		</nav>
 	)
