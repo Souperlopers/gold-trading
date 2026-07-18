9ffd3af5a53a2552df031b662b9cef74dee80c5c
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-18 20:20:55 +0330

    update(Logo): add link to home

--- nextjs/app/_components/ui/Logo.tsx
+++ nextjs/app/_components/ui/Logo.tsx
@@ -1,6 +1,7 @@
+import Link from "next/link"
+
 export default function Logo() {
 	return (
-		<div className="w-12.5 h-12.5 bg-primary rounded-full shrink-0">
-		</div>
+		<Link href='/' className="w-12.5 h-12.5 bg-primary rounded-full shrink-0"></Link>
 	)
 }
