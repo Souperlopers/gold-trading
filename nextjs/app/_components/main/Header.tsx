76bd51f6510c0e265fca341738ced7e50ac5a5a4
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-18 20:09:31 +0330

    fix(Header): prevent login button from overflowing in tablet mode

--- nextjs/app/_components/main/Header.tsx
+++ nextjs/app/_components/main/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
 			dir="rtl"
 			className="bg-base-200 h-18.75 sticky top-0 w-full border-b-3 border-x rounded-b-xl border-base-300 flex justify-center"
 		>
-			<div className="px-10 flex items-center justify-between gap-16 w-full h-full max-w-360">
+			<div className="px-10 flex items-center justify-between w-full h-full max-w-360">
 				<Logo />
 				<Navigation />
 				<div className="flex justify-between gap-10 items-center">
