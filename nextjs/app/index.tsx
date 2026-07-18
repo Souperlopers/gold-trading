a8c03a6a02e6b9a625c2ea30ed509be42330a562
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-18 16:33:03 +0330

    delete index.ts

--- nextjs/app/index.tsx
+++
@@ -1,5 +0,0 @@
-//main
-export {default as Header} from "@/app/_components/main/Header";
-export {default as Navigation} from "@/app/_components/main/NavigationMenu";
-export {default as Logo} from "@/app/_components/main/Logo";
-export {default as ThemeSwitch} from "@/app/_components/main/ThemeSwitcher";
