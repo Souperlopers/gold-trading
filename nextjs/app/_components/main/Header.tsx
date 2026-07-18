256e3da0a69d9d797cc3b6c3e6c4f24207b94a88
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2026-07-18 12:32:01 +0330

    moved logo and themeswitch to ui folder

--- nextjs/app/_components/main/Header.tsx
+++ nextjs/app/_components/main/Header.tsx
@@ -1,13 +1,21 @@
-import { Logo, ThemeSwitch } from "@/app/index";
-import Navigation from "@/app/_components/main/NavigationMenu";
+import Navigation from "@/app/_components/main/NavigationMenu"
+import ProfileButton from "@/app/_components/ui/ProfileButton"
+import Logo from "@/app/_components/ui/Logo"
+import ThemeSwitch from "@/app/_components/ui/ThemeSwitch"
 
 export default function Header() {
 	return (
-		<header dir="rtl" className="bg-base-200 h-18.75 sticky top-0 w-full border-b-3 border-x rounded-b-xl border-base-300 flex justify-center">
+		<header
+			dir="rtl"
+			className="bg-base-200 h-18.75 sticky top-0 w-full border-b-3 border-x rounded-b-xl border-base-300 flex justify-center"
+		>
 			<div className="px-10 flex items-center justify-between gap-16 w-full h-full max-w-360">
 				<Logo />
 				<Navigation />
-				<ThemeSwitch />
+				<div className="flex justify-between gap-10 items-center">
+					<ThemeSwitch />
+					<ProfileButton />
+				</div>
 			</div>
 		</header>
 	)
