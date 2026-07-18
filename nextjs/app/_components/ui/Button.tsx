beb94412092f2923fd7ac8718c00f295c7e17b41
Zahra Rostami <zahrarostami137878@gmail.com>
2026-07-18 16:34:57 +0330

    update(Button): convert Arrow Function to Function Declaration

--- nextjs/app/_components/ui/Button.tsx
+++ nextjs/app/_components/ui/Button.tsx
@@ -5,7 +5,7 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &{
     title: string;
 }
 
-const Button = ({title, onClick, disabled}:ButtonProps) => {
+export default function Button ({title, onClick, disabled}:ButtonProps){
     return (
         <button type='button' onClick={onClick} disabled={disabled} 
         className={`p-3 w-68 h-10 flex justify-center items-center bg-primary hover:bg-primary-hover text-accent-dark rounded-lg border-surface font-medium transition-colors duration-300`}>
@@ -13,5 +13,3 @@ const Button = ({title, onClick, disabled}:ButtonProps) => {
         </button>
     );
 }
-
-export default Button;
